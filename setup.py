dbf2dd19fd5994cba41714c5521a89b6a5c58866
Kartik Nimhan <kartiknimhan9929@gmail.com>
2024-06-10 17:14:59 +0530

    after adding

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages,setup
 #from typing import List 
 
 
+
+
+
 setup(
     name='sensor',
     version="0.0.1",
@@ -9,4 +12,5 @@ setup(
     author_email="kartiknimhan9929@gmail.com",
     packages=find_packages(),
     install_requires=["pymongo"]
+    
 )
